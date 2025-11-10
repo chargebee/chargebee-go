fd0dce40c674971bd53fff48cf3b9466530a8616
cb-karthikp <karthikp@chargebee.com>
2025-11-10 11:48:11 +0530

    add webhook parser

---
+++ webhook/parser.go
@@ -0,0 +1,32 @@
+package webhook
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/chargebee/chargebee-go/v3"
+	"github.com/chargebee/chargebee-go/v3/models/event"
+	"strings"
+)
+
+func Content(event event.Event) *chargebee.Result {
+	content := &chargebee.Result{}
+	err1 := json.Unmarshal(event.Content, content)
+	if err1 != nil {
+		panic(err1)
+	}
+	return content
+}
+
+func Deserialize(jsonObj string) *event.Event {
+	event := &event.Event{}
+	err := json.Unmarshal([]byte(jsonObj), event)
+	if err != nil {
+		panic(errors.New("Response not in JSON format. Might not be a ChargeBee webhook call"))
+	}
+	apiVersion := event.ApiVersion
+	envVersion := chargebee.APIVersion
+	if apiVersion != "" && strings.ToUpper(string(apiVersion)) != strings.ToUpper(envVersion) {
+		panic(errors.New("API version [" + strings.ToUpper(string(apiVersion)) + "] in response does not match with client library API version [" + strings.ToUpper(envVersion) + "]"))
+	}
+	return event
+}
